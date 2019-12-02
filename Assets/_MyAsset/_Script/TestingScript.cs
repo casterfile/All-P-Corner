fa725c216b14d1702116a3bdb2ad78681f2e0e85
tonyMac <tonyMac@tonys-MacBook-Pro.local>
2019-12-02 14:43:30 +0800

    first commit

---
+++ Assets/_MyAsset/_Script/TestingScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+	public static bool isTesting = false;
+	// Use this for initialization
+	void Awake () {
+		#if UNITY_EDITOR
+		isTesting = false;
+		#endif	
+
+		//print ("isTesting: "+ isTesting);
+	}
+}
